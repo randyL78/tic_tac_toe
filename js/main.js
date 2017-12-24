11bb37d090c6354af96560f5e62e84251ff9aff1
RandyL <30959856+randyL78@users.noreply.github.com>
2017-12-23 20:18:10 -0500

    Added placing of O onto squares

--- js/main.js
+++ js/main.js
@@ -1,73 +1,145 @@
 /* jshint esversion: 6 */
 
-// Wait until HTML loads to begin program
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
++function() {
 
-    //=============================
-    //  Globales
-    //=============================
-  
+    // Wait until HTML loads to begin program
+    document.addEventListener('DOMContentLoaded', () => {
+        'use strict'; 
 
-    //=============================
-    //  Generic helper functions
-    //=============================
-    /**
-     * Hide an element
-     * @param {object} element  The element to hide
-     */
-    const hideElement = (element) => {
-        element.classList.add("disappear");
-     };
- 
-     /**
-      * Show an element
-      * @param {object} element  The element to show
-      */
-     const showElement = (element) => {
-         element.classList.remove("hide");
-      };
+        //=============================
+        //  Generic helper functions
+        //=============================
+        /**
+         * Hide an element
+         * @param {object} element  The element to hide
+         */
+        const hideElement = (element) => {
+            element.classList.add("hide");
+        };
+    
+        /**
+         * Show an element
+        * @param {object} element  The element to show
+        */
+        const showElement = (element) => {
+            element.classList.remove("hide");
+        };
+        /**
+        * Make an element "disappear"
+        * @param {object} element  The element to show
+        */
+        const disappearElement = (element) => {
+            element.classList.add("disappear");
+            // after the css transition time, hide the element completly
+            setTimeout(() => {
+                hideElement(element);
+                element.classList.remove("disappear");
+            }, 2000);
+        };
 
 
-    // =============================
-    //   Load Screen
-    // =============================
- 
-
-    function createLoadScreen() {
-        const loadScreen = document.createElement("div");
-        loadScreen.className = "screen screen-start";
-        loadScreen.setAttribute("id", "start");
-        loadScreen.innerHTML = `
-            <header>
-                <h1>Tic Tac Toe</h1>
-                <a href="#" class="button">Start game</a>
-            </header>`;
-        document.querySelector("body").appendChild(loadScreen);
-
-        loadScreen.querySelector(".button").onclick = () => hideElement(loadScreen);
-        return loadScreen;
-    }
-    createLoadScreen();
+        // =============================
+        //   Load Screen
+        // =============================
     
 
+        const createLoadScreen = () => {
+            const loadScreen = document.createElement("div");
+            loadScreen.className = "screen screen-start";
+            loadScreen.setAttribute("id", "start");
+            loadScreen.innerHTML = `
+                <header>
+                    <h1>Tic Tac Toe</h1>
+                    <a href="#" class="button">Start game</a>
+                </header>`;
+            document.querySelector("body").appendChild(loadScreen);
 
-    // =============================
-    //   Game Logic
-    // =============================
-    let playerIndex = 0;
-    class Player{ 
-        constructor(name, active) {
-        this.name = (name) ? name : "Player" + playerIndex;
-        this.active = active;
+            loadScreen.querySelector(".button").onclick = () => disappearElement(loadScreen);
+            return loadScreen;
         }
-    }
-
-    function setPlayerTurn(player) {
+        createLoadScreen();
         
 
-    }
+
+        // =============================
+        //   Game Logic
+        // =============================
+        const boxes = document.querySelector(".boxes");
+
+
+        boxes.onclick = (e) => {
+            board.forEach( (square) => {
+                if (e.target === square.element) {
+                   if (square.ownedBy === "none") square.ownedBy = "1";                  
+                }
+            });
+        }
+
+        boxes.onmouseover = (e) => {
+            if (e.target.classList.contains("box") ) {
+                e.target.classList.add("box-filled-1");
+            };
+        };
+
+        boxes.onmouseout = (e) => {
+            board.forEach( (square) => {
+                if (e.target === square.element) {
+                   if (square.ownedBy === "none") e.target.classList.remove("box-filled-1");                
+                }
+            });
+        };
+
+        const getBoxLoc = (hitBox) => {
+            // boxArray.forEach( (box, index) => {
+            //     if (hitBox === box) {
+            //         const row = Math.floor(index / 3);
+            //         const col = index % 3;
+            //         return {row: row, col: col};
+            //     }
+            // });
+            return false;        
+        }
+
+        // =============================
+        //   Players
+        // =============================
+
+        // =============================
+        //   Board
+        // ============================= 
+        function Square(location, ownedBy = "none") {
+            this.location = location;
+            this.ownedBy = ownedBy;
+            this.element = boxes.querySelectorAll(".box")[(this.location.row * 3) + this.location.column ];
+        }
 
 
 
-});
+        const buildBoard = () => {
+            let tempArray = [];
+            for (let r = 0; r < 3; r++) {
+                for (let c = 0; c < 3; c++) {
+                    tempArray.push(new Square({row: r, column: c}));
+                }  
+            }
+            return tempArray;
+
+        };
+
+        // const buildColumn = (row) => {
+        //     let tempArray = [];
+        //     for (let j = 0; j < 3; j++) {
+        //         tempArray.push(new Square([row, j]));
+        //     }  
+        //     return tempArray;
+        // }
+        const board = buildBoard();
+
+
+        // =============================
+        //   AI
+        // =============================
+
+
+    });
+}();
